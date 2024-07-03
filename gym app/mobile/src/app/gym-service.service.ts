56b6d3e47c6eda7285f9063cb2b382fe04328925
alpinerum <35735448+alpinerum@users.noreply.github.com>
2024-07-02 21:22:39 -0400

    implemented integration between services backend and frontend

--- gym app/mobile/src/app/gym-service.service.ts
+++ gym app/mobile/src/app/gym-service.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { gymService } from './gymService';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GymServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getServices():Observable<gymService[]> {
+    const headers = new HttpHeaders({'Content-Type': 'application/json', 'Accept': 'application/json'});
+    const options = {headers: headers};
+    return this.http.get<gymService[]>('http://localhost:3000/services/getServices', options);
+  }
+  addService(form: any):Observable<gymService[]> {
+    const headers = new HttpHeaders({'Content-Type': 'application/json', 'Accept': 'application/json'});
+    const options = {headers: headers};
+    return this.http.post<gymService[]>('http://localhost:3000/services/addService', form, options);
+  }
 }
--- gym app/mobile/src/app/tab3/tab3.page.ts
+++ gym app/mobile/src/app/tab3/tab3.page.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gymService } from '../gymService';
+import { GymServiceService } from '../gym-service.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-
-  constructor() {}
+export class Tab3Page implements OnInit {
+services: gymService[] = [];
+  constructor(private service: GymServiceService) {}
+  ngOnInit(): void {
+    this.service.getServices().subscribe((res)=>this.services=res);
+  }
 
 }
