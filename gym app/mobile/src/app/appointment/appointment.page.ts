9cd9785356a1bab256d8aed84aa023fa09665fb7
alpinerum <35735448+alpinerum@users.noreply.github.com>
2024-07-07 15:17:09 -0400

    started implementing appointment form

--- gym app/mobile/src/app/appointment/appointment.page.ts
+++ gym app/mobile/src/app/appointment/appointment.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment',
   templateUrl: './appointment.page.html',
   styleUrls: ['./appointment.page.scss'],
 })
-export class AppointmentPage implements OnInit {
+export class AppointmentPage {
+  form: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required])
+  });
 
   constructor() { }
 
-  ngOnInit() {
+  get name() {
+    return this.form.get('name');
   }
 
+  get email() {
+    return this.form.get('email');
+  }
+
+  get category() {
+    return this.form.get('category');
+  }
+
+  get description() {
+    return this.form.get('description');
+  }
+
+  submitForm() {
+    
+  }
 }
